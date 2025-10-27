6ce6271d7dd4a3feef91a53e70b2e495408ad958
Deepak Kumar Mishra <demishra@microsoft.com>
2025-10-27 09:46:36 +0530

    Day19-JSEvents

--- Day19/custom/custom-event.js
+++ Day19/custom/custom-event.js
@@ -1,3 +1,37 @@
 console.log("Custom Events in JavaScript");
 
 // Creating a custom event
+
+
+// step 1: create a custom event
+const myEvent = new CustomEvent("userLoggedIn", {
+    detail:{
+        username: "deepak",
+        role: "admin"
+    }
+});
+
+// step 2: listen to the custom event
+document.addEventListener("userLoggedIn", function(event) {
+    console.log("User Logged In:");
+    console.log("Username:", event.detail.username);
+    console.log("Role:", event.detail.role);
+});
+
+//step 3: Dispatching the custom event
+document.dispatchEvent(myEvent);
+
+function loginUser(username) {
+    const event = new CustomEvent("userLoggedIn", {
+        detail: {
+            username
+            
+}
+    });
+    document.dispatchEvent(event);
+}
+
+document.addEventListener("userLoggedIn", function(event) {
+    const user = event.detail.username;
+    document.getElementById("welcome").textContent = `Welcome, ${user}!`;
+});
